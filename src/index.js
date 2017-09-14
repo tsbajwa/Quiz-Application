d80932372cde8c066131931ff5e309c874112692
tsbajwa <teranjit@gmail.com>
2017-09-14 10:47:19 +1000

    create react app cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+const App = () => (
+  <div>
+    App component
+  </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
