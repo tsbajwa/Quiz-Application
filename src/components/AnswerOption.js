1fea35f0d15db7d85db73e18c438bcf0e9372a33
tsbajwa <teranjit@gmail.com>
2017-09-18 12:56:18 +1000

    cleanup

--- src/components/AnswerOption.js
+++ src/components/AnswerOption.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function AnswerOption(props) {
   let check = null;
-
   if (props.answerSelected !== "") {
     if (props.answer === props.answerText) {
       check = "Correct";
@@ -11,14 +10,6 @@ export default function AnswerOption(props) {
       check = "Incorrect";
     }
   }
-
-  // if (props.answerSelected === props.answerText) {
-  //   if (props.answerSelected === props.answer) {
-  //     check = "CORRECT";
-  //   } else {
-  //     check = "INCORRECT";
-  //   }
-  // }
   return (
     <div onClick={() => props.onClick(props.answerText)}>
       <p>
@@ -28,8 +19,3 @@ export default function AnswerOption(props) {
     </div>
   );
 }
-
-//Need to highlight correct answer
-// if props.answerSelected !== ''
-//if prop.answerSelected === props.answer
-//check Correct
