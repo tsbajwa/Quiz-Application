6ddf780bc5e30f30740ba48b272184a1f3090def
tsbajwa <teranjit@gmail.com>
2017-10-22 18:48:44 +1100

    basic styling complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,16 @@ import { NavLink, Link } from "react-router-dom";
 export default function NavBar({ isAuthed }) {
   return (
     <header className="nav">
-      <Link className="nav__brand--link" to="/">
-        <span className="nav__brand--name">Brand Name</span>
-      </Link>
-      <input type="checkbox" id="nav__accordian" class="nav__accordian--checkbox" />
-      <label for="nav__accordian" class="nav__accordian--label">
-        <i />
-        <i />
-        <i />
+      <div className="nav__brandContainer">
+        <Link className="nav__brand--link" to="/">
+          <span className="nav__brand--name">Brand Name</span>
+        </Link>
+      </div>
+
+      <label htmlFor="unique" className="nav__accordian--label">
+        &equiv;
       </label>
+      <input type="checkbox" id="unique" className="nav__accordian--checkbox" />
       {isAuthed ? (
         <div className="nav__ul">
           <NavLink to="/quiz" className="nav__li">
