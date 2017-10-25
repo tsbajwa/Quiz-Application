5d266461b0df14d2fd99faca9df3723e730772d5
tsbajwa <teranjit@gmail.com>
2017-10-25 13:52:08 +1100

    Simplified QuizButton. Stateless fn component

--- src/components/QuizButton.js
+++ src/components/QuizButton.js
@@ -1,44 +1,36 @@
 import React from "react";
 import { QuizButtonRender } from "../components";
 import { Link } from "react-router-dom";
-export default class QuizButton extends React.Component {
-  renderButtons = () => {
-    const showPrevBtn = this.props.currentQuestionIndex === 0 ? false : true;
-    const showNextBtn =
-      this.props.currentQuestionIndex > this.props.lastAnsweredQuestionIndex ? false : true;
-    const quizFinished =
-      this.props.lastAnsweredQuestionIndex + 1 === this.props.quizLength ? true : false;
+export default function QuizButton(props) {
+  const showPrevBtn = props.currentQuestionIndex === 0 ? false : true;
+  const showNextBtn = props.currentQuestionIndex > props.lastAnsweredQuestionIndex ? false : true;
+  const quizFinished = props.lastAnsweredQuestionIndex + 1 === props.quizLength ? true : false;
 
-    if (quizFinished) {
-      //Can add in previous button here still
-      return (
-        <div>
-          <Link to="/results">See Results</Link>
-        </div>
-      );
-    } else {
-      return (
-        <div className="quizButton__container">
+  if (quizFinished) {
+    //Can add in previous button here still
+    return (
+      <div>
+        <Link to="/results">See Results</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="quizButton__container">
+        <QuizButtonRender
+          enableButton={showPrevBtn}
+          className="quizButton__btn--prev"
+          onClick={props.handlePreviousButtonClick}
+          text="Previous"
+        />
+        {showNextBtn ? (
           <QuizButtonRender
-            enableButton={showPrevBtn}
-            className="quizButton__btn--prev"
-            onClick={this.props.handlePreviousButtonClick}
-            text="Previous"
+            enableButton={showNextBtn}
+            className="quizButton__btn--next"
+            text="Next"
+            onClick={props.handleNextButtonClick}
           />
-          {showNextBtn ? (
-            <QuizButtonRender
-              enableButton={showNextBtn}
-              className="quizButton__btn--next"
-              text="Next"
-              onClick={this.props.handleNextButtonClick}
-            />
-          ) : null}
-        </div>
-      );
-    }
-  };
-
-  render() {
-    return <div>{this.renderButtons()}</div>;
+        ) : null}
+      </div>
+    );
   }
 }
