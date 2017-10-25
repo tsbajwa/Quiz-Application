a2c4180268f3781e66ac515cbdb6cc05b6292183
tsbajwa <teranjit@gmail.com>
2017-10-25 14:19:32 +1100

    refactor of QuizContainer

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { AnswerOptionContainer } from "../containers";
+import { AnswerOptionContainer, InfoBarContainer, QuizButtonContainer } from "../containers";
 
 export default function Quiz({ questionText, answerArray }) {
   return (
     <div className="quiz__container">
+      <InfoBarContainer />
       <p>{questionText}</p>
       {answerArray.map((answerOption, index) => (
         <AnswerOptionContainer answerOption={answerOption} key={index} />
       ))}
+      <QuizButtonContainer />
     </div>
   );
 }
--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Quiz } from "../components";
-import { InfoBarContainer, QuizButtonContainer } from "../containers";
 import { connect } from "react-redux";
 
 class QuizContainer extends React.Component {
   render() {
     const answerArray = Object.entries(this.props.answerOptions).map(arr => arr[1]);
-    return (
-      <div className="quizContainer__container">
-        <InfoBarContainer />
-        <Quiz answerArray={answerArray} questionText={this.props.question} />
-        <QuizButtonContainer />
-      </div>
-    );
+    return <Quiz answerArray={answerArray} questionText={this.props.question} />;
   }
 }
 
