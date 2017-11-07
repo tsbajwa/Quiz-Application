8a2bbbdfc4e5ab9fc8373357983a865382a3f17c
tsbajwa <teranjit@gmail.com>
2017-11-07 17:03:18 +1100

    Basic styling for nav component complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FaBars from "react-icons/lib/fa/bars";
+
 export default function NavBar(props) {
   const blockName = props.width > 500 ? "navWide" : "navMobile";
   return (
-    <nav>
-      <div className={blockName}>
+    <nav className={blockName}>
+      <div className="nav-container">
         <div className="nav__brand">
           <Link className="nav__brand--link" to="/">
             <span className="nav__brand--name">Brand Name</span>
           </Link>
         </div>
-        <div>
-          {blockName === "navMobile" ? <i onClick={props.handleMenuToggle}>Hey Hey</i> : null}
-          {props.renderNavLinks(blockName)}
+        <div className="nav__icon">
+          {blockName === "navMobile" ? (
+            <FaBars color="white" size={30} onClick={props.handleMenuToggle} />
+          ) : null}
         </div>
       </div>
+      {props.renderNavLinks(blockName)}
     </nav>
   );
 }
-
-//Send the prop back up
